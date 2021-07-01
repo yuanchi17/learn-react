b5c93ac3ff1f4d472b5bc0f55de44b96637f6760
李元琦 <yuan.chi@program.com.tw>
2021-07-01 14:34:48 +0800

    useEffect

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
@@ -6,6 +6,16 @@ const Counter = () => {
     setCount((newCount, newProps) => newCount + 1)
     setCount((newCount, newProps) => newCount + 1)
   }
+
+  useEffect(() => {
+    console.log('「元件渲染完成」')
+    console.log(`因第一次渲染畫面及 count 更新時觸發。count = ${count}`)
+    return () => {
+      console.log('「元件被移除前觸發 return」')
+      console.log(`舊值為: ${count}`)
+    }
+  }, [count])
+
   return (
     <div>
       <p>點擊次數：{count}</p>
