e987fe4b6a64a2344130cd6ff0df9c129321f0cb
李元琦 <yuan.chi@program.com.tw>
2021-07-14 17:44:47 +0800

    News「狀態提升」

--- src/views/News/News.jsx
+++ src/views/News/News.jsx
@@ -1,19 +1,29 @@
 import { Route, Switch } from 'react-router-dom'
 import NewsList from './NewsList.jsx'
 import NewsContent from './NewsContent.jsx'
-import React from 'react'
+import React, { useState } from 'react'
 
-const News = () => (
-  <Switch>
-    <Route exact path="/news" component={() => (
-      <div>
-        <h2>最新消息</h2>
-        <NewsList />
-      </div>
-    )}/>
-    <Route path="/news/content/:id" component={NewsContent}/>
-
-  </Switch>
-)
+const News = () => {
+  const [NewsItems] = useState([
+    { id: 1, title: '我是新聞 1', text: '今天天氣晴' },
+    { id: 2, title: '我是新聞 2', text: '齊心防疫，同島一心' },
+    { id: 3, title: '我是新聞 3', text: '居家防疫' },
+    { id: 4, title: '我是新聞 4', text: '希望疫情趕快結束！' },
+  ])
+  return (
+    <Switch>
+      <Route exact path="/news" component={() => (
+        <div>
+          <h2>最新消息</h2>
+          <NewsList news={NewsItems}/>
+        </div>
+      )}/>
+      <Route
+        path="/news/content/:id"
+        component={props => <NewsContent news={NewsItems} match={props.match}/>}
+      />
+    </Switch>
+  )
+}
 
 export default News
--- src/views/News/NewsContent.jsx
+++ src/views/News/NewsContent.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Content = props => {
   console.log(props.match)
+  const id = props.match.params.id
+  const news = props.news.find(news => String(news.id) === String(id))
   return (
   <div>
     <h3>你正在閱讀訊息</h3>
+    <p>{news.text}</p>
   </div>
   )
 }
