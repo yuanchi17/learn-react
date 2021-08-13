b28e0d82b461dd84037c98a9d10a50d5dac116e4
李元琦 <yuan.chi@program.com.tw>
2021-08-13 10:56:09 +0800

    useDispatch

--- src/views/News/NewsList.jsx
+++ src/views/News/NewsList.jsx
@@ -1,27 +1,23 @@
-import { connect } from 'react-redux'
 import { delNewsObj } from '../../actions/news'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import React from 'react'
 
-const mapStateToProps = state => ({
-  news: state.news,
-})
+const List = () => {
+  const news = useSelector(state => state.news)
+  const dispatch = useDispatch()
+  return (
+    <ul>
+      {
+        news.map(n => (
+          <li key={n.id}>
+            <Link to={`/news/content/${n.id}`}>{n.title}</Link>
+            <button onClick={() => dispatch(delNewsObj(n.id)) }>刪除</button>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
 
-const mapDispatchToProps = dispatch => ({
-  delNews: nid => {
-    dispatch(delNewsObj(nid))
-  },
-})
-
-const List = props => (
-  <ul>
-    {props.news.map(news => (
-      <li key={news.id}>
-        <Link to={`/news/content/${news.id}`}>{news.title}</Link>
-        <button onClick={() => props.delNews(news.id)}>刪除</button>
-      </li>
-    ))}
-  </ul>
-)
-
-export default connect(mapStateToProps, mapDispatchToProps)(List)
+export default List
