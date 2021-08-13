c298fc3c6c4bfdc6df5096e0943ab1118fbaf7d0
李元琦 <yuan.chi@program.com.tw>
2021-08-13 11:39:56 +0800

    middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,20 @@
-import { createStore } from 'redux'
+import _ from 'lodash'
+import { createStore, applyMiddleware } from 'redux'
 import news from './news'
 
-const store = createStore(news)
+const logger = store => next => action => {
+  console.log(`執行: ${_.get(action, 'type', '未知')}`, action)
+  console.log('執行前的 state:', store.getState())
+
+  const result = next(action)
+
+  console.log('執行後的 state:', store.getState())
+  return result
+}
+
+const store = createStore(
+  news,
+  applyMiddleware(logger),
+)
 
 export default store
